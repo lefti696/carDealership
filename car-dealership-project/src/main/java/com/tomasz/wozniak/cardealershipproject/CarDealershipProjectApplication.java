d303cd2455cace5eac2125c71bbbf3b34b48fa20
Tomek <lefti696@yahoo.com>
2018-07-26 23:15:57 +0200

    initial commit, fresh spring initializr

---
+++ car-dealership-project/src/main/java/com/tomasz/wozniak/cardealershipproject/CarDealershipProjectApplication.java
@@ -0,0 +1,12 @@
+package com.tomasz.wozniak.cardealershipproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipProjectApplication.class, args);
+	}
+}
